9fdc641ecb464fb2c43af1336ceb0b4facd6f337
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-12 17:59:55 +0200

    gradually add more functionality

--- app.py
+++ app.py
@@ -7,39 +7,39 @@ from dateutil.relativedelta import relativedelta
 def handler(event, context):
 
 
-    # def get_query_parameter(param: str):
-    #     query_string_parameters = event["queryStringParameters"]
-    #     if query_string_parameters is None:
-    #         raise AssertionError(f'querystring does not contain any parameters')
-    #     if param not in query_string_parameters:
-    #         raise AssertionError(
-    #             f'querystring must contain a {param} parameter')
-    #     return query_string_parameters[param]
+    def get_query_parameter(param: str):
+        if "queryStringParameters" not in event:
+            raise AssertionError(f'contains not querystring key')
+        query_string_parameters = event["queryStringParameters"]
+        if query_string_parameters is None:
+            raise AssertionError(f'querystring does not contain any parameters')
+        if param not in query_string_parameters:
+            raise AssertionError(
+                f'querystring must contain a {param} parameter')
+        return query_string_parameters[param]
 
-    # try:
-    #     ticker = get_query_parameter('ticker')
-    #     start_year = int(get_query_parameter('start_year'))
-    #     end_year = int(get_query_parameter('end_year'))
-    #     investing_years = int(get_query_parameter('investing_years'))
-    # except AssertionError as e:
-    #     return {
-    #         'statusCode': 400,
-    #         'headers': {
-    #             'Access-Control-Allow-Headers': 'Content-Type',
-    #             'Access-Control-Allow-Origin': '*',
-    #             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-    #         },
-    #         'body': json.dumps({'error': str(e)})
-    #     }
+    try:
+        ticker = get_query_parameter('ticker')
+        start_year = int(get_query_parameter('start_year'))
+        end_year = int(get_query_parameter('end_year'))
+        investing_years = int(get_query_parameter('investing_years'))
+    except AssertionError as e:
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps({'error': str(e)})
+        }
     
     ticker = 'MSFT'
-    start_year = 2000
-    end_year = 2002
     investing_years=1
     
     
     investingModel = investing.InvestingModel()
-    investingModel.set_ticker(ticker)  # S&P500
+    investingModel.set_ticker(ticker)
     investingModel.set_interval_years(start_year, end_year)
     timeseries = investingModel.get_timeseries()
     gains = investingModel.calculate_distribution(strategy_fn=strategies.lump_sum_gain,
