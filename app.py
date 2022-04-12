824de255efa3aa6ba657c201a75091baf0bce934
Teun Schaeken <teun.schaeken@gmail.com>
2022-04-12 14:47:22 +0200

    check docker aws via gh actions

---
+++ app.py
@@ -0,0 +1,14 @@
+import json
+
+def handler(event, context):
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps({'i am active': 'and working', 
+                            'response': event})
+    }
